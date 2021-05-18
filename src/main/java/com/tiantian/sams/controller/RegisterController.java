b80c62f6ec62130310d1be82d33f63b0a9529461
tiantian <724513626@qq.com>
2021-05-18 23:00:32 +0800

    更新文档

--- src/main/java/com/tiantian/sams/controller/RegisterController.java
+++ src/main/java/com/tiantian/sams/controller/RegisterController.java
@@ -11,8 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpSession;
-
 @Controller
 public class RegisterController {
 
